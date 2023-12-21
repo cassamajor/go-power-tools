d8a9a2d1a8b887d000f33f824116930184782e8c
Steven Cassamajor <contributor@networkaxis.org>
2023-12-21 18:30:40 -0500

    hello-pkg: Implement option constructor (aka functional options)

--- hello/hello.go
+++ hello/hello.go
@@ -1,23 +1,57 @@
 package hello
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
 type Printer struct {
 	Output io.Writer
 }
 
-func NewPrinter() *Printer {
-	return &Printer{Output: os.Stdout}
+type option func(*Printer) error
+
+func WithOutput(w io.Writer) option {
+	return func(p *Printer) error {
+		if w == nil {
+			return errors.New("nil is not a valid writer")
+		}
+		p.Output = w
+		return nil
+	}
 }
 
-func (p *Printer) Print() {
+func NewPrinter(option ...option) (*Printer, error) {
+	p := &Printer{Output: os.Stdout}
+
+	for _, opt := range option {
+		err := opt(p)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return p, nil
+}
+
+func (p *Printer) Print() string {
 	fmt.Fprintln(p.Output, "Hello, world")
+
+	s := new(strings.Builder)
+	fmt.Fprint(s, p.Output) // Write the content of io.Writer to the string builder.
+
+	return s.String()
 }
 
 func DefaultPrinter() {
-	NewPrinter().Print()
+	p, err := NewPrinter()
+
+	if err != nil {
+		panic(err)
+	}
+
+	p.Print()
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -8,15 +8,30 @@ import (
 
 func TestPrintTo_PrintsHelloMessageToGivenWriter(t *testing.T) {
 	t.Parallel()
-	buf := new(bytes.Buffer)
 
-	p := hello.NewPrinter()
-	p.Output = buf
-	p.Print()
+	tests := []struct {
+		name   string
+		option hello.Printer
+		want   string
+	}{
+		{
+			name: "Print Hello Message To Given Writer",
+			option: hello.Printer{
+				Output: new(bytes.Buffer),
+			},
+			want: "Hello, world\n",
+		},
+	}
 
-	want := "Hello, world\n"
-	got := buf.String()
-	if want != got {
-		t.Errorf("want %q, got %q", want, got)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			output := hello.WithOutput(tt.option.Output)
+			p, _ := hello.NewPrinter(output)
+
+			if got := p.Print(); got != tt.want {
+				t.Errorf("got = %v, want = %v", got, tt.want)
+			}
+		})
 	}
 }
