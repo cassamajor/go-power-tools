3824ca749cd7c222167d54eaf0292e129cc253ac
Steven Cassamajor <contributor@networkaxis.org>
2023-12-20 22:38:04 -0500

    greet-pkg: Implement option constructor (aka functional options)

--- greet/greet.go
+++ greet/greet.go
@@ -2,36 +2,80 @@ package greet
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
-type Prompter struct {
-	Input  io.Reader
-	Output io.Writer
+type prompter struct {
+	input  io.Reader
+	output io.Writer
 }
 
-func NewPrompter() *Prompter {
-	return &Prompter{
-		Input:  os.Stdin,
-		Output: os.Stdout,
+type option func(*prompter) error
+
+func WithInput(r io.Reader) option {
+	return func(p *prompter) error {
+		if r == nil {
+			return errors.New("nil is not a valid reader")
+		}
+		p.input = r
+		return nil
 	}
 }
 
-func (p *Prompter) Prompt() {
+func WithOutput(w io.Writer) option {
+	return func(p *prompter) error {
+		if w == nil {
+			return errors.New("nil is not a valid writer")
+		}
+		p.output = w
+		return nil
+	}
+}
+
+func NewPrompter(option ...option) (*prompter, error) {
+	p := &prompter{
+		input:  os.Stdin,
+		output: os.Stdout,
+	}
+
+	for _, opt := range option {
+		err := opt(p)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return p, nil
+}
+
+func (p *prompter) Prompt() string {
 	name := "Stranger"
 
-	fmt.Fprintln(p.Output, "What is your name?")
-	input := bufio.NewScanner(p.Input)
+	fmt.Fprintln(p.output, "What is your name?")
+	input := bufio.NewScanner(p.input)
 
 	if input.Scan() {
 		name = input.Text()
 	}
 
-	fmt.Fprintf(p.Output, "Hello, %v\n", name)
+	fmt.Fprintf(p.output, "Hello, %v\n", name)
+
+	s := new(strings.Builder)
+	fmt.Fprint(s, p.output) // Write the content of io.Writer to the string builder.
+
+	return s.String()
 }
 
-func Main() {
-	NewPrompter().Prompt()
+func DefaultPrompt() {
+	c, err := NewPrompter()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintf(c.output, c.Prompt())
 }
--- greet/greet_test.go
+++ greet/greet_test.go
@@ -4,14 +4,20 @@ import (
 	"bytes"
 	"errors"
 	"github.com/cassamajor/greet"
+	"io"
 	"testing"
 	"testing/iotest"
 )
 
 type promptTest struct {
-	name string
-	args greet.Prompter
-	want string
+	name   string
+	option args
+	want   string
+}
+
+type args struct {
+	input  io.Reader
+	output *bytes.Buffer
 }
 
 func TestPrompt(t *testing.T) {
@@ -20,17 +26,17 @@ func TestPrompt(t *testing.T) {
 	tests := []promptTest{
 		{
 			name: "Prompts user for a name and renders greeting",
-			args: greet.Prompter{
-				Input:  bytes.NewBufferString("Steven"),
-				Output: new(bytes.Buffer),
+			option: args{
+				input:  bytes.NewBufferString("Steven"),
+				output: new(bytes.Buffer),
 			},
 			want: "What is your name?\nHello, Steven\n",
 		},
 		{
 			name: "Prints Hello, stranger on read error",
-			args: greet.Prompter{
-				Input:  iotest.ErrReader(errors.New("bad reader")),
-				Output: new(bytes.Buffer),
+			option: args{
+				input:  iotest.ErrReader(errors.New("bad reader")),
+				output: new(bytes.Buffer),
 			},
 			want: "What is your name?\nHello, Stranger\n",
 		},
@@ -38,15 +44,14 @@ func TestPrompt(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			//t.Parallel()
-			// Using tt.args.stdout is not concurrency-safe.
-			// When tests are run in parallel, each test is simultaneously trying to set tt.args.stdout to a different writer to print the results.
+			// Using tt.option.output is not concurrency-safe.
+			// When tests are run in parallel, each test is simultaneously trying to set tt.option.output to a different writer to print the results.
 
-			p := greet.NewPrompter()
-			p.Input = tt.args.Input
-			p.Output = tt.args.Output
-			p.Prompt()
+			input := greet.WithInput(tt.option.input)
+			output := greet.WithOutput(tt.option.output)
+			p, _ := greet.NewPrompter(input, output)
 
-			if got := p.Output.String(); got != tt.want {
+			if got := p.Prompt(); got != tt.want {
 				t.Errorf("got = %v, want %v", got, tt.want)
 			}
 		})
