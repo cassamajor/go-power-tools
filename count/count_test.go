4bece3fb9b6bd24c2c9d2a871dfedc6ef0a4192f
Steven Cassamajor <contributor@networkaxis.org>
2023-12-28 21:10:19 -0500

    count-pkg: Remove redundant test

--- count/count_test.go
+++ count/count_test.go
@@ -7,43 +7,7 @@ import (
 	"testing"
 )
 
-type args struct {
-	Input  io.Reader
-	Output *bytes.Buffer
-}
-
 func TestCounter(t *testing.T) {
-	t.Parallel()
-
-	tests := []struct {
-		name string
-		args args
-		want int
-	}{
-		{
-			name: "Count the newlines in the provided input",
-			args: args{
-				Input:  bytes.NewBufferString("1\n2\n3\n"),
-				Output: new(bytes.Buffer),
-			},
-			want: 3,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
-
-			input := count.WithInput(tt.args.Input)
-			c, _ := count.NewCounter(input)
-
-			if got := c.Count(); got != tt.want {
-				t.Errorf("got = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestCounterWithArgs(t *testing.T) {
 	t.Parallel()
 
 	tests := []struct {
